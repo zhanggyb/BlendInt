df37c648b208b409268d479502ecef17a7d0ddbb
Freeman Zhang <zhanggyb@gmail.com>
2013-07-15 14:53:10 +0800

    Port texture_atlas in freetype-gl to C++, need unit test

---
+++ include/BIL/TextureAtlas.h
@@ -0,0 +1,85 @@
+/*
+ * TextureAtlas.h
+ *
+ *  Created on: 2013年7月15日
+ *      Author: zhanggyb
+ */
+
+/* Modified from freetype-gl project */
+
+#ifndef _BIL_TEXTUREATLAS_H_
+#define _BIL_TEXTUREATLAS_H_
+
+#include <vector>
+#include <stdlib.h>
+
+#include <BIL/Tuple.h>
+
+using namespace std;
+
+namespace BIL {
+
+	class TextureAtlas
+	{
+	public:
+
+		TextureAtlas (const size_t w, const size_t h, const size_t d);
+
+		virtual ~TextureAtlas ();
+
+		/**
+		 * @brief Upload atlas to video memory
+		 */
+		void upload (void);
+
+		/**
+		 * @brief Allocate a new region in the atlas
+		 * @param w width of the region to allocate
+		 * @param h height of the region to allocate
+		 * @return Coordinates of the allocated region
+		 */
+		Tuple4i getRegion (const size_t w, const size_t h);
+
+		// void setRegion
+		/**
+		 * @brief Upload data to the specified atlas region
+		 * @param x
+		 */
+		void setRegion (const size_t x, const size_t y, const size_t w,
+		        const size_t h, const unsigned char* data, const size_t stride);
+
+		/**
+		 * @brief Remove all allocated regions from the atlas.
+		 */
+		void clear (void);
+
+	private:
+
+		int fit (const size_t index, const size_t w, const size_t h);
+
+		void merge (void);
+
+		/** Allocated nodes */
+		vector<Tuple3i> _nodes;
+
+		/** Width (in pixels) of the underlying texture */
+		size_t _width;
+
+		/** Height (in pixels) of the underlying texture */
+		size_t _height;
+
+		/** Depth (in bytes) of the underlying texture */
+		size_t _depth;
+
+		/** Allocated surface size */
+		size_t _used;
+
+		/** Texture identity (OpenGL) */
+		unsigned int _id;
+
+		/** Atlas data */
+		unsigned char* _data;
+	};
+
+} /* namespace BIL */
+#endif /* TEXTUREATLAS_H_ */
---
+++ src/TextureAtlas.cpp
@@ -0,0 +1,242 @@
+/*
+ * TextureAtlas.cpp
+ *
+ *  Created on: 2013年7月15日
+ *      Author: zhanggyb
+ */
+
+#include <GL/gl.h>
+#include <stdio.h>
+#include <string.h>
+#include <assert.h>
+#include <algorithm>
+#include <limits.h>
+
+#include <BIL/TextureAtlas.h>
+
+namespace BIL {
+
+	TextureAtlas::TextureAtlas (const size_t w, const size_t h, const size_t d)
+			: _width(w), _height(h), _depth(d), _used(0), _id(0)
+	{
+		Tuple3<int> node = { { 1, 1, w - 2 } };
+
+		assert((d == 1) || (d == 3) || (d == 4));
+
+		_nodes.push_back(node);
+
+		_data = (unsigned char*) calloc(_width * _height * _depth,
+		        sizeof(unsigned char));
+
+		if (_data == NULL)
+			throw;
+	}
+
+	TextureAtlas::~TextureAtlas ()
+	{
+		_nodes.clear();
+
+		if (_data) {
+			free(_data);
+		}
+		if (_id) {
+			glDeleteTextures(1, &_id);
+		}
+	}
+
+	void TextureAtlas::setRegion (const size_t x, const size_t y,
+	        const size_t w, const size_t h, const unsigned char* data,
+	        const size_t stride)
+	{
+		size_t i, depth, charsize;
+
+		assert(x > 0);
+		assert(y > 0);
+		assert(x < (_width - 1));
+		assert((x + w) <= (_width - 1));
+		assert(y < (_height - 1));
+		assert((y + h) <= (_height - 1));
+
+		depth = _depth;
+		charsize = sizeof(char);
+
+		for (i = 0; i < h; ++i) {
+			memcpy(_data + ((y + i) * _width + x) * charsize * depth,
+			        data + (i * stride) * charsize, w * charsize * depth);
+		}
+	}
+
+	Tuple4i TextureAtlas::getRegion (const size_t w, const size_t h)
+	{
+		Tuple4i region = { { 0, 0, w, h } };
+		int y, best_height, best_width, best_index;
+		Tuple3i node, prev;
+		size_t i;
+
+		best_height = INT_MAX;
+		best_index = -1;
+		best_width = INT_MAX;
+
+		for (i = 0; i < _nodes.size(); ++i) {
+			y = fit(i, w, h);
+			if (y >= 0) {
+				node = _nodes[i];
+				if (((y + h) < best_height)
+				        || (((y + h) == best_height) && (node.z < best_width))) {
+					best_height = y + h;
+					best_index = i;
+					best_width = node.z;
+					region.x = node.x;
+					region.y = y;
+				}
+			}
+		}
+
+		if (best_index == -1) {
+			region.x = -1;
+			region.y = -1;
+			region.width = 0;
+			region.height = 0;
+			return region;
+		}
+
+		node.x = region.x;
+		node.y = region.y + h;
+		node.z = w;
+
+		// TODO: insert node at best_index;
+		vector<Tuple3i>::iterator it = _nodes.begin();
+		advance(it, best_index);
+		it = _nodes.insert(it, node);
+
+		for (it = it + 1, i = best_index + 1; i < _nodes.size(); ++i) {
+			node = *it;
+			prev = *(it - 1);
+
+			if (node.x < (prev.x + prev.z)) {
+				int shrink = prev.x + prev.z - node.x;
+				node.x += shrink;
+				node.z -= shrink;
+				if (node.z <= 0) {
+					it = _nodes.erase(it);
+					--i;
+				} else {
+					break;
+				}
+			} else {
+				break;
+			}
+		}
+
+		merge();
+		_used += w * h;
+
+		return region;
+	}
+
+	void TextureAtlas::clear (void)
+	{
+		Tuple3i node = { { 1, 1, 1 } };
+
+		assert(_data);
+
+		_nodes.clear();
+		_used = 0;
+		node.z = _width - 2;
+
+		_nodes.push_back(node);
+		memset(_data, 0, _width * _height * _depth);
+	}
+
+	void TextureAtlas::upload (void)
+	{
+		assert(_data);
+
+		if (!_id) {
+			glGenTextures(1, &_id);
+		}
+
+		glBindTexture( GL_TEXTURE_2D, _id);
+		glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+		glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+		glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		if (_depth == 4) {
+#ifdef GL_UNSIGNED_INT_8_8_8_8_REV
+			glTexImage2D( GL_TEXTURE_2D, 0, GL_RGBA, _width, _height, 0,
+			        GL_BGRA, GL_UNSIGNED_INT_8_8_8_8_REV, _data);
+#else
+			glTexImage2D( GL_TEXTURE_2D, 0, GL_RGBA, _width, _height,
+					0, GL_RGBA, GL_UNSIGNED_BYTE, _data );
+#endif
+		} else if (_depth == 3) {
+			glTexImage2D( GL_TEXTURE_2D, 0, GL_RGB, _width, _height, 0, GL_RGB,
+			        GL_UNSIGNED_BYTE, _data);
+		} else {
+			glTexImage2D( GL_TEXTURE_2D, 0, GL_ALPHA, _width, _height, 0,
+			        GL_ALPHA, GL_UNSIGNED_BYTE, _data);
+		}
+	}
+
+	int TextureAtlas::fit (const size_t index, const size_t w, const size_t h)
+	{
+		Tuple3i node;
+		int x, y, width_left;
+		size_t i;
+
+		node = _nodes[index];
+		x = node.x;
+		y = node.y;
+		width_left = w;
+		i = index;
+
+		if ((x + w) > (_width - 1))
+			return -1;
+
+		while (width_left > 0) {
+			node = _nodes[i];
+			if (node.y > y) {
+				y = node.y;
+			}
+			if ((y + h) > (_height - 1))
+				return -1;
+			width_left -= node.z;
+			++i;
+		}
+		return y;
+	}
+
+	void TextureAtlas::merge (void)
+	{
+		Tuple3i node, next;
+		size_t i;
+		vector<Tuple3i>::iterator it;
+
+		/*
+		 for (i = 0; i < _nodes.size() - 1; ++i)
+		 {
+		 node = _nodes[i];
+		 next = _nodes[i + 1];
+		 if (node.y == next.y) {
+		 node.z += next.z;
+		 it = find (_nodes, next);
+		 _nodes.erase(it);
+		 --i;
+		 }
+		 }*/
+
+		for (it = _nodes.begin(), i = 0; i < _nodes.size() - 1; ++i) {
+			// TODO: check the following lines are correct
+			node = *it;
+			next = *(it + 1);
+			if (node.y == next.y) {
+				node.z += next.z;
+				it = _nodes.erase(it + 1);
+				--i;
+			} else {
+				it++;
+			}
+		}
+	}
+
+} /* namespace BIL */
