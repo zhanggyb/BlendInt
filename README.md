5c1bf98157e27d101b8f4c9620f93c23c80f6cff
Freeman Zhang <zhanggyb@gmail.com>
2013-08-09 15:08:23 +0800

    add LGPL v3 license file and update the comments in source code.

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-glfwbil
-=======
+BIL
+===
+
+BIL (Blender Interface Library), is a C++ library for generating
+applications with Blender-like interface style, licensed with LGPL
+v3+.
+
+## Usage
+
+## Requirements
+
+## Installation
+
+## Contributing
+
+## Running the Tests
+
+## Credits
+
+## License
+
+BIL (Blender Interface Library) is released under the LGPL v3
+License. See the bundled LICENSE file for details.
--- include/BIL/Label.h
+++ include/BIL/Label.h
@@ -1,8 +1,22 @@
 /*
- * Label.h
+ * This file is part of BIL (Blender Interface Library).
  *
- *	Created on: 2013年7月11日
- *		Author: zhanggyb
+ * BIL (Blender Interface Library) is free software: you can
+ * redistribute it and/or modify it under the terms of the GNU Lesser
+ * General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * BIL (Blender Interface Library) is distributed in the hope that it
+ * will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with BIL.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ * Contributor(s): Freeman Zhang <zhanggyb@gmail.com>
  */
 
 #ifndef _BIL_LABEL_H_
