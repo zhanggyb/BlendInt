cdf6ade6ff4d34900d34a7557e7be6ec394137b9
Freeman Zhang <zhanggyb@gmail.com>
2013-09-27 18:00:30 +0800

    make glut demo work

--- demo/glut/main.cpp
+++ demo/glut/main.cpp
@@ -7,43 +7,58 @@
 #include <GL/glut.h>
 /* ADD GLOBAL VARIABLES HERE LATER */
 
+#include <BILO/Interface.hpp>
+#include <BILO/Label.hpp>
+
 int init_resources(void)
 {
-	/* FILLED IN LATER */
+	using BILO::Interface;
+	using BILO::Label;
 	
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	glOrtho(0.f, 1200, 0.f, 800, 100.f, -100.f);
+	Interface::initialize();
+	Interface* app = Interface::instance();
+	app->resize(1200, 800);
+	
+	// 	glMatrixMode(GL_PROJECTION);
+	// 	glLoadIdentity();
+	// 	glOrtho(0.f, 1200, 0.f, 800, 100.f, -100.f);
+	
+	Label* label = new Label("Hello World!");
+	label->set_pos (200, 200);
+		
+	app->bind(label);
+	
 	return 1;
 }
 
 void onDisplay()
 {
-	glClearColor(1.0, 1.0, 1.0, 1.0);
-	glClear(GL_COLOR_BUFFER_BIT);
+	using BILO::Interface;
+	
+	Interface::instance()->render();
 	
 	/* FILLED IN LATER */
-	glMatrixMode(GL_MODELVIEW);
-	glPushMatrix();
-	
-	glTranslatef(500,
-				 200,
-			  0);
-	
-	glEnable(GL_BLEND);
-	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-	
-	glColor3ub(255, 200, 133);
-	
-	glBegin(GL_TRIANGLES);
-	glVertex2f (200, 200);
-	glVertex2f (400, 200);
-	glVertex2f (300, 300);
-	glEnd();
-	
-	glDisable(GL_BLEND);
-	
-	glPopMatrix();
+// 	glMatrixMode(GL_MODELVIEW);
+// 	glPushMatrix();
+// 	
+// 	glTranslatef(500,
+// 				 200,
+// 			  0);
+// 	
+// 	glEnable(GL_BLEND);
+// 	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+// 	
+// 	glColor3ub(255, 200, 133);
+// 	
+// 	glBegin(GL_TRIANGLES);
+// 	glVertex2f (200, 200);
+// 	glVertex2f (400, 200);
+// 	glVertex2f (300, 300);
+// 	glEnd();
+// 	
+// 	glDisable(GL_BLEND);
+// 	
+// 	glPopMatrix();
 	
 	glutSwapBuffers();
 }
@@ -54,34 +69,43 @@ void onIdle() {
 
 void free_resources()
 {
-	/* FILLED IN LATER */
+	using BILO::Interface;
+	
+	Interface::release();
+}
+
+void onReshape (int width, int height)
+{
+	using BILO::Interface;
+	
+	Interface::instance()->resize(width, height);
+}
+
+void onMouse (int button, int state, int x, int y)
+{
+	using BILO::Interface;
+	
+	//std::cout << "mouse position: "
+	//BILO::Interface::instance()->mouseButtonEvent(button, action, mods);
 }
 
 int main(int argc, char* argv[])
 {
+	BILO_EVENTS_INIT_ONCE_IN_MAIN;
+	
 	/* Glut-related initialising functions */
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_RGBA|GLUT_ALPHA|GLUT_DOUBLE|GLUT_DEPTH);
 	glutInitWindowSize(1200, 800);
-	glutCreateWindow("My First Triangle");
-	
-	GLenum glew_status = glewInit();
-	if (glew_status != GLEW_OK) {
-		fprintf(stderr, "Error: %s\n", glewGetErrorString(glew_status));
-		return 1;
-	}
-	
-	if (!GLEW_VERSION_2_0) {
-		fprintf(stderr, "Error: your graphic card does not support OpenGL 2.0\n");
-		return 1;
-	}
-	
+	glutCreateWindow("GLUT Demo");
+
 	/* When all init functions run without errors,
 	 * the program can initialise the resources */
 	if (1 == init_resources())
 	{
 		/* We can display it if everything goes OK */
 		glutDisplayFunc(onDisplay);
+		glutReshapeFunc(onReshape);
 		glutIdleFunc(onIdle);
 		glutMainLoop();
 	}
