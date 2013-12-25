86b6b47975c6bcb13d235447e5e57676b0780146
Freeman Zhang <zhanggyb@gmail.com>
2013-12-25 17:32:29 +0800

    add doc comments

--- include/BlendInt/Menu.hpp
+++ include/BlendInt/Menu.hpp
@@ -32,8 +32,7 @@
 namespace BlendInt {
 
 	/**
-	 * @brief The menu widget for use in menu bars, context menus,
-	 * and other popup menus.
+	 * @brief A class to store menu items
 	 */
 	class Menu
 	{
@@ -41,33 +40,87 @@ namespace BlendInt {
 
 		friend class MenuItem;
 
+		/**
+		 * @brief Default constructor
+		 */
 		Menu ();
 
+		/**
+		 * @brief Constructor with title
+		 * @param[in] title The title of this menu
+		 */
 		Menu (const String& title);
 
+		/**
+		 * @brief Destructor
+		 */
 		~Menu();
 
+		/**
+		 * @brief Set the title of this menu
+		 * @param[in] title The title of this menu
+		 *
+		 * This inline function just simply set the title of the menu.
+		 */
 		inline void set_title (const String& title)
 		{
 			m_title = title;
 		}
 
+		/**
+		 * @brief Get the title of this menu
+		 * @return The title string
+		 */
 		inline const String& title () const {return m_title;}
 
+		/**
+		 * @brief Get the size of menu items
+		 * @return The size of menu items
+		 */
 		inline size_t size () const {return m_list.size();}
 
+		/**
+		 * @brief Get the reference of menu item list
+		 * @return Reference of the std::list member
+		 */
 		inline std::list<MenuItem*>& list () {return m_list;}
 
+		/**
+		 * @brief Add a menu item and set the text
+		 * @param[in] text The text string shown in the menu item
+		 */
 		void Add (const String& text);
 
+		/**
+		 *
+		 */
 		void Add (FormBase* icon, const String& text);
 
 		void Add (MenuItem* item);
 
+		/**
+		 * @brief Set the parent MenuItem of this menu
+		 * @param[in] item The parent MenuItem
+		 */
 		void SetParent (MenuItem* item);
 
+		/**
+		 * @brief Remove a MenuItem from this menu
+		 * @param[in] item The MenuItem object need to be removed
+		 *
+		 * @note This function will not delete the item removed, do to so, use Menu::Delete instead
+		 * or delete it manually.
+		 *
+		 * @sa Delete
+		 */
 		void Remove (MenuItem* item);
 
+		/**
+		 * @brief Remove and delete a MenuItem from this menu
+		 * @param[in] item The MenuItem object need to be deleted
+		 *
+		 * @sa Remove
+		 */
 		void Delete (MenuItem* item);
 
 #ifdef DEBUG
